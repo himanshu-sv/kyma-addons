3b9b021b4ee5be945fd4f667515621e94424932a
i303609 <himanshu.sharma04@sap.com>
2020-01-19 15:52:38 +0100

    cds

--- addons/cds-connector-0.0.1/components/lambda/lambda.js
+++ addons/cds-connector-0.0.1/components/lambda/lambda.js
@@ -11,9 +11,5 @@ module.exports = { main: async function (event, context) {
             data:event.data
         });
         console.log("response" + JSON.stringify(postEventResponse))
-        const Url = process.env.GATEWAY_URL + "/profile/htp741143850/profiles/44a14fe9-adb9-4fbd-ad2f-4c9d415fbcff";
-        console.log( "sending get to:", Url);
-        const { data: result } = await axios.get(Url, {});
-        console.log(result);
-        return result;
+        return postEventResponse;
     } }
